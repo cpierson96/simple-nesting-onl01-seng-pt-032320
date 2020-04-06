7ad53623b0e1aadcfdb73391e7fa5681dfcef480
Claudia Pierson <c172@umbc.edu>
2020-04-06 03:03:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,11 +1,19 @@
-def hopper
+ef hopper
 	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-    
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
 
 
 end
