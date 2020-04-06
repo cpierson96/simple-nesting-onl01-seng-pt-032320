f8b7379a34bfe8bbba142f6f2990fd64d040665c
Claudia Pierson <c172@umbc.edu>
2020-04-06 03:23:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
     }
 
 end
-"Smalltalk", "LISP",
+#"Smalltalk", "LISP",
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
