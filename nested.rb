7b6c4bc8751c8f1f169298b31dcf566c9aca27a8
Claudia Pierson <c172@umbc.edu>
2020-04-06 03:38:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,19 +14,20 @@ def alan_kay_is_known_for
 	
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
+        #:grace_hopper => {
+         # :known_for => "COBOL",
+          #:languages => ["COBOL", "FORTRAN"]
+        #},
         :alan_kay => {
           :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
+          #:languages => ["Smalltalk", "LISP"]
         },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
+       # :dennis_ritchie => {
+        #  :known_for => "Unix",
+        #  :languages => ["C"]
         }
      }
+     
 end
 
 def dennis_ritchies_language
