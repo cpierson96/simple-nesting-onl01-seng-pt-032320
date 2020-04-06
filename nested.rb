90988e618135e2c6436867c09ff5952243648c44
Claudia Pierson <c172@umbc.edu>
2020-04-06 04:03:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,6 +110,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     adding_to_dennis[:dennis_ritchie][:languages]).to include("Assembly")
+     programmer_hash[:dennis_ritchie][:languages]).to include("Assembly")
 
 end
